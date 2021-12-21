2ce9943bdecdbdd287710293279d57796f1b8639
TatyanaMatsvey <tetyana.matsvey@gmail.com>
2021-12-21 19:51:10 +0200

    В этом коммите: - Были заменены find elements + time.sleep() на ожидания (waiters) - Был написан декоратор в новом файле until.py (пока еще не примененный, нужно разобраться в чем причина). - Обновленные тесты исправно работают

--- pages/base.py
+++ pages/base.py
@@ -1,15 +1,34 @@
 import logging
 
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+
+from selenium.webdriver.support.wait import WebDriverWait
+
 
 class BasePage:
 
     def __init__(self, driver):
         self.driver = driver
         self.log = logging.getLogger(__name__)
+        self.wait = WebDriverWait(driver, timeout=5)
 
-    def fill_field(self, by, locator, value):
+    def fill_field(self, locator, value, by=By.XPATH):
         """Fill field using provided variables"""
+        username = self.wait_until_find_element(by=by, value=locator)
 
-        username = self.driver.find_element(by=by, value=locator)
         username.clear()
         username.send_keys(value)
+
+    def wait_until_find_element(self, value, by=By.XPATH):
+        """Wait until find element"""
+        return self.wait.until(EC.presence_of_element_located(locator=(by, value)))
+
+    def wait_until_element_enabled(self, value, by=By.XPATH):
+        """Wait until element enabled"""
+        element = self.wait_until_find_element(by=by, value=value)
+        return self.wait.until(EC.element_to_be_clickable(element))
+
+    def wait_until_element_disappear(self, value, by=By.XPATH):
+        """Wait until element is not visible anymore"""
+        self.wait.until_not(EC.visibility_of_element_located(locator=(by, value)))
