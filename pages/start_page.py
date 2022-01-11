65711e3434313b6317f2a918b2a675d06875d5af
TatyanaMatsvey <tetyana.matsvey@gmail.com>
2022-01-11 18:23:32 +0200

    В этом коммите: - Были исправлены веитеры; - Добавлены и применены декораторы; - Обновленные тесты исправно работают.

--- pages/start_page.py
+++ pages/start_page.py
@@ -3,11 +3,8 @@ import random
 from selenium.webdriver.common.by import By
 from constants.start_page import StartPageConstants
 from pages.base import BasePage
-
-
-def random_num():
-    """Generate random number"""
-    return str(random.choice(range(11111, 99999)))
+from pages.main_page import MainPage
+from pages.utils import log_decorator
 
 
 class StartPage(BasePage):
@@ -15,34 +12,43 @@ class StartPage(BasePage):
     def __init__(self, driver):
         super().__init__(driver)
         self.constants = StartPageConstants()
-        self.log = logging.getLogger(__name__)
 
+    @log_decorator
     def login(self, username_value, password_value):
         """Login using provided password and username"""
 
-        self.fill_field(by=By.XPATH, locator=self.constants.SIGN_IN_USERNAME_XPATH, value=username_value)
-        self.fill_field(by=By.XPATH, locator=self.constants.SIGN_IN_PASSWORD_XPATH, value=password_value)
+        self.fill_field(locator=self.constants.SIGN_IN_USERNAME_XPATH, value=username_value)
+        self.fill_field(locator=self.constants.SIGN_IN_PASSWORD_XPATH, value=password_value)
         self.log.debug("fields are filled with invalid values")
 
-        button = self.driver.find_element(by=By.XPATH, value=self.constants.SIGN_IN_BUTTON_XPATH)
+        # Click on Sign In button
+        button = self.wait_until_element_enabled(value=self.constants.SIGN_IN_BUTTON_XPATH)
         button.click()
+        self.log.debug("clicked on Sign In")
+        return MainPage(self.driver)
 
+    @log_decorator
     def verify_incorrect_login(self):
         """Verify error message om invalid login"""
-        message = self.driver.find_element(by=By.XPATH, value=self.constants.SIGN_IN_ERROR_MESSAGE_XPATH)
-        assert message.text == self.constants.SIGN_IN_ERROR_MESSAGE_TEXT
 
+        # Find error message
+        message = self.wait_until_element_enabled(value=self.constants.SIGN_IN_ERROR_MESSAGE_XPATH)
+        # Verify message
+        assert message.text == self.constants.SIGN_IN_ERROR_MESSAGE_TEXT
+        return self
+
+    @log_decorator
     def test_link(self):
         """Link in start page is displayed"""
-        link = self.driver.find_element(by=By.XPATH, value=self.constants.LINK_XPATH)
+        link = self.wait_until_find_element(value=self.constants.LINK_XPATH)
         link.is_displayed()
 
     def h1_on_page(self):
         """h1 on start page is displayed"""
-        h1 = self.driver.find_element(by=By.XPATH, value=self.constants.H1_XPATH)
+        h1 = self.wait_until_find_element(value=self.constants.H1_XPATH)
         h1.is_displayed()
 
     def placeholder(self):
         """Link in start page is displayed"""
-        placeholder = self.driver.find_element(by=By.XPATH, value=self.constants.SIGN_IN_PLACEHOLDER_XPATH)
+        placeholder = self.wait_until_find_element(value=self.constants.SIGN_IN_PLACEHOLDER_XPATH)
         placeholder.is_displayed()
